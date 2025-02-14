b8b6fed9edada1db07cd5f54d5fa51d091aa6fea
soudip-maity <soudipmaity605@gmail.com>
2025-02-14 22:46:10 +0530

    COMMIT

--- sidebar.js
+++ sidebar.js
@@ -1,18 +1,24 @@
 let mode = document.getElementById("mode");
-let maindiv = document.getElementById("maindiv")
-let container= document.getElementById("container")
+let maindiv = document.getElementById("maindiv");
+let container = document.getElementById("container");
+let dark_mode = document.getElementsByClassName("dark-mode")
 
-  mode.addEventListener("click",  async () => {
-
-    if (maindiv.classList.contains("dark-mode")) {
-
-      maindiv.classList.remove("dark-mode");
+mode.addEventListener("click", async () => {
+  if (maindiv.classList.contains("dark-mode")) {
+    maindiv.classList.remove("dark-mode");
+    if (container) {
       container.classList.remove("dark-mode");
-      maindiv.style.background = ''; 
      
-    } else {
-      
-      maindiv.classList.add("dark-mode");
-      maindiv.style.background = "linear-gradient(to right, #200122, #6f0000)";
     }
-  });   
+  } else {
+    maindiv.classList.add("dark-mode");
+    maindiv.style.background = "linear-gradient(to right, #200122, #6f0000)";
+  }
+
+  
+
+
+
+
+
+});
