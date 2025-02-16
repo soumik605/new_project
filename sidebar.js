c696b317a59639cda65783ddbeb1f48c0fab860c
abdul-ajim-khan <aak8389966@gmail.com>
2025-02-17 00:37:59 +0530

    10th commit

--- sidebar.js
+++ sidebar.js
@@ -3,19 +3,19 @@ let maindiv = document.getElementById("maindiv");
 let container = document.getElementById("container");
 let dark_mode = document.getElementsByClassName("dark-mode")
 
-mode.addEventListener("click", async () => {
-  if (maindiv.classList.contains("dark-mode")) {
-    maindiv.classList.remove("dark-mode");
-    if (container) {
-      container.classList.remove("dark-mode");
+// mode.addEventListener("click", async () => {
+//   if (maindiv.classList.contains("dark-mode")) {
+//     maindiv.classList.remove("dark-mode");
+//     if (container) {
+//       container.classList.remove("dark-mode");
      
-    }
-  } else {
-    maindiv.classList.add("dark-mode");
-    maindiv.style.background = "linear-gradient(to right, #200122, #6f0000)";
-  }
+//     }
+//   } else {
+//     maindiv.classList.add("dark-mode");
+//     maindiv.style.background = "linear-gradient(to right, #200122, #6f0000)";
+//   }
 
-});
+// });
 
 
   // mode.addEventListener("click",  () => {
