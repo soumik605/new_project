2286329e735228e503d38f5e351787f6917235c5
abdul-ajim-khan <aak8389966@gmail.com>
2025-02-18 16:31:22 +0530

    commit

--- sidebar.js
+++ sidebar.js
@@ -35,23 +35,20 @@ let dark_mode = document.getElementsByClassName("dark-mode")
 
 
 
-
-
-
-let links = document.querySelectorAll(".button a");
-let currentPage = window.top.location.pathname.split("/").pop();
-console.log(currentPage);
-document.querySelectorAll(".button").forEach(button => {
-  let link = button.querySelector("a");
-    let linkPage = link.getAttribute("href").split("/").pop();
-    if (currentPage === linkPage) {
-      button.classList.add("active");
-    } else {
-        button.classList.remove("active");
-    }
+document.addEventListener("DOMContentLoaded", function () {
+    let currentPage = window.top.location.pathname.split("/").pop();
+    document.querySelectorAll(".button").forEach(button => {
+        let link = button.parentElement; 
+        if (link && link.tagName === "A") { 
+            let linkPage = link.getAttribute("href").split("/").pop();
+            if (currentPage === linkPage) {
+                button.classList.add("active");
+            } else {
+                button.classList.remove("active");
+            }
+        }
+    });
 });
-
-
 
 
 
