87d1adc0235f0c02cf2d6f01aba0b07ca69ce645
abdul-ajim-khan <aak8389966@gmail.com>
2025-02-25 12:47:29 +0530

    commit

--- posts.js
+++ posts.js
@@ -1,216 +1,118 @@
+// window onload function
 window.onload = function () {
   let maincont = document.getElementById("maincont");
-  let load = document.createElement("div");
-  load.classList.add("load");
-  maincont.appendChild(load);
   posts();
 };
 
 let currentPage = 1;
 const limit = 10;
-let postsData = []; 
+let postsData = [];
 
 async function posts() {
-
   let maincont = document.getElementById("maincont");
   let users = [];
-  // let exportButton = document.createElement("button");
-  // exportButton.id = "exportButton";
-  // exportButton.innerText = "Export to CSV";
 
-  let overlay= document.createElement("div")
-  overlay.id="overlay"
-  overlay.classList.add("hide")
+  // Modal / overlay
+  let overlay = document.createElement("div");
+  overlay.id = "overlay";
+  overlay.classList.add("hide");
   let modal = document.createElement("div");
-  modal.id="modal"
-  modal.classList.add("hide")
-  let closeBtn= document.createElement("button")
-  closeBtn.id="closeBtn"
-  modal.appendChild(closeBtn)
-  closeBtn.innerHTML=`X`
+  modal.id = "modal";
+  modal.classList.add("hide");
+  let closeBtn = document.createElement("button");
+  closeBtn.id = "closeBtn";
+  closeBtn.innerHTML = "X";
+  modal.appendChild(closeBtn);
 
-
-  // let container = document.createElement("div");
-  // container.id = "container";
-  // container.className = "dark-mode";
-  
-  let load = document.createElement("div");
-  load.classList.add("load");
-  container.appendChild(load);
-
-  
   let exportButton = document.createElement("button");
   exportButton.id = "exportButton";
   exportButton.innerText = "Export to CSV";
   maincont.appendChild(exportButton);
-  
+
   exportButton.addEventListener("click", () => {
     exportToCSV(postsData);
   });
+
   let container = document.createElement("div");
   container.id = "container";
- 
+
   await fetch("https://dummyjson.com/posts")
     .then((response) => response.json())
     .then((json) => (users = json.posts));
+
   maincont.innerHTML = "";
   maincont.appendChild(exportButton);
   maincont.appendChild(modal);
   maincont.appendChild(overlay);
-
   maincont.appendChild(container);
 
-    let postsData = [];
-
-    
-  let loadButton = document.createElement("button");
-  loadButton.textContent = "Load more...";
-  loadButton.classList.add("loadButton");
-  maincont.appendChild(loadButton);
-  
-  loadButton.addEventListener("click", () => {
-    loadMorePosts();
-  });
-
-  container.innerHTML = ""; 
-  await loadMorePosts(); 
-}
-
-async function loadMorePosts() {
-  let container = document.getElementById("container");
-  
-  let response = await fetch(`https://dummyjson.com/posts?limit=${limit}&skip=${(currentPage - 1) * limit}`);
-  let data = await response.json();
-
-  if (!data.posts || data.posts.length === 0) {
-    document.querySelector(".loadButton").style.display = "none"; 
-    return;
-  }}
-
-  for (const user of data.posts) {
+  users.forEach(async (user, idx) => {
+    let mainUserdiv = document.createElement("div");
+    mainUserdiv.classList.add("mainUserdiv");
     let userDiv = document.createElement("div");
     userDiv.classList.add("userDiv");
+    container.appendChild(mainUserdiv);
+    mainUserdiv.appendChild(userDiv);
 
-    let userInfo = await infos(user.userId);
-
+    let userInfo = await infos(user.id);
     let title = user.title;
     let body = user.body;
     let tags = user.tags;
     let likes = user.reactions.likes;
     let dislikes = user.reactions.dislikes;
+    
+    userDiv.addEventListener("click", openModal);
+    overlay.addEventListener("click", closeModal);
+    closeBtn.addEventListener("click", closeModal);
 
-    users.forEach(async (user, idx) => {
-      let mainUserdiv = document.createElement("div");
-      mainUserdiv.classList.add("mainUserdiv");
-      container.appendChild(mainUserdiv);
-      let userDiv = document.createElement("div");
-      userDiv.id = `user_index-${idx}`;
-      userDiv.classList.add("userDiv");
-      mainUserdiv.appendChild(userDiv);
-     
+    let titleDiv = document.createElement("div");
+    titleDiv.classList.add("titlediv");
+    titleDiv.innerText = `Title: ${title}`;
+    userDiv.appendChild(titleDiv);
 
-      let userName = await infos(user.id);
-      let title = user.title;
-      let body = user.body;
-      let tags = user.tags;
-      let likes = user.reactions.likes;
-      let dislikes = user.reactions.dislikes;
+    let bodyDiv = document.createElement("div");
+    bodyDiv.classList.add("bodydiv");
+    const words = body.split(" ");
+    const first20Words = words.slice(0, 20).join(" ");
+    bodyDiv.innerText = first20Words + "...";
+    userDiv.appendChild(bodyDiv);
 
-      userDiv.addEventListener("click", openModal, container.style.cursor="none",);
-      overlay.addEventListener("click", closeModal);
-      closeBtn.addEventListener("click", closeModal);
-    let namediv = document.createElement("div");
-    namediv.classList.add("namediv");
-    namediv.innerText = `${userInfo.fullname}`;
-    userDiv.appendChild(namediv);
+    let reaction = document.createElement("div");
+    reaction.classList.add("reaction");
+    userDiv.appendChild(reaction);
 
+    let likesDiv = document.createElement("div");
+    likesDiv.classList.add("likesdiv");
+    likesDiv.innerText = `Likes: ${likes}`;
+    reaction.appendChild(likesDiv);
 
-      let titleDiv = document.createElement("div");
-      titleDiv.classList.add("titlediv");
-      titleDiv.innerText = `Title: ${title}`;
-      userDiv.appendChild(titleDiv);
+    let dislikesDiv = document.createElement("div");
+    dislikesDiv.classList.add("dislikesdiv");
+    dislikesDiv.innerText = `Dislikes: ${dislikes}`;
+    reaction.appendChild(dislikesDiv);
 
-      let bodyDiv = document.createElement("div");
-      bodyDiv.classList.add("bodydiv");
-      const words = body.split(" ");
-      const first20Words = words.slice(0, 20).join(" ");
-      bodyDiv.innerText = first20Words + "...";
-      userDiv.appendChild(bodyDiv);
+    let tagsDiv = document.createElement("div");
+    tagsDiv.classList.add("tagsdiv");
+    tagsDiv.innerText = `Tags: ${tags.join(", ")}`;
+    userDiv.appendChild(tagsDiv);
 
-      // let namediv = document.createElement("div");
-      // namediv.classList.add("namediv");
-      // namediv.innerText = `${userName}`;
-      // userDiv.appendChild(namediv);
-
-      let reaction = document.createElement("div");
-      reaction.classList.add("reaction");
-      userDiv.appendChild(reaction);
-    container.appendChild(userDiv);
-
-  //   postsData.push({
-  //     title: title,
-  //     userName: userInfo.fullname,
-  //     bodyPreview: first20Words + "...",
-  //     likes: likes,
-  //     dislikes: dislikes,
-  //     tags: tags.join(", "),
-  //   });
-  // })
-
-      let likesDiv = document.createElement("div");
-      likesDiv.classList.add("likesdiv");
-      likesDiv.innerText = `Likes: ${likes}`;
-      reaction.appendChild(likesDiv);
-      likesDiv.innerText = `Likes: ${likes}`;
-
-      let dislikesDiv = document.createElement("div");
-      dislikesDiv.classList.add("dislikesdiv");
-      dislikesDiv.innerText = `Dislikes: ${dislikes}`;
-      reaction.appendChild(dislikesDiv);
-      dislikesDiv.innerText = `dislikes: ${dislikes}`;
-
-      let tagsDiv = document.createElement("div");
-      tagsDiv.classList.add("tagsdiv");
-      tagsDiv.innerText = `Tags: ${tags.join(", ")}`;
-      userDiv.appendChild(tagsDiv);
-
-      postsData.push({
-        title: title,
-        userName: userName,
-        bodyPreview: first20Words + "...",
-        likes: likes,
-        dislikes: dislikes,
-        tags: tags.join(", "),
-      });
+    postsData.push({
+      title: title,
+      userName: userInfo.fullname,
+      bodyPreview: first20Words + "...",
+      likes: likes,
+      dislikes: dislikes,
+      tags: tags.join(", "),
     });
-    let loadButton = document.createElement("button");
-    loadButton.textContent = "Load more...";
-    loadButton.classList.add("loadButton");
-    container.appendChild(loadButton);
-
-    let user_index = 0;
-    function showMoreUsers() {
-      for (let i = user_index; i < user_index + 10 && i < users.length; i++) {
-        let userr = document.getElementById(`user_index-${i}`);
-        if (userr) {
-          userr.style.display = "block";
-        }
-      }
-      user_index += 10;
-      if (user_index === users.length) {
-        loadButton.style.display = "none";
-      }
-    }
-
-    showMoreUsers();
-
-    loadButton.addEventListener("click", showMoreUsers);
-  
+  });
 }
 
+// User data from user id
 async function infos(user_id) {
-  let user_info = await fetch(`https://dummyjson.com/users/${user_id}`).then(response => response.json());
-  
+  let user_info = await fetch(`https://dummyjson.com/users/${user_id}`).then(
+    (response) => response.json()
+  );
+
   return {
     fullname: `${user_info.firstName} ${user_info.lastName}`,
     email: user_info.email,
@@ -220,9 +122,17 @@ async function infos(user_id) {
   };
 }
 
+// CSV data download
 function exportToCSV(data) {
-  const csvHeaders = ["Title", "User Name", "Body Preview", "Likes", "Dislikes", "Tags"];
-  const csvRows = data.map(item => {
+  const csvHeaders = [
+    "Title",
+    "User Name",
+    "Body Preview",
+    "Likes",
+    "Dislikes",
+    "Tags",
+  ];
+  const csvRows = data.map((item) => {
     return `"${item.title}","${item.userName}","${item.bodyPreview}","${item.likes}","${item.dislikes}","${item.tags}"`;
   });
 
@@ -234,19 +144,18 @@ function exportToCSV(data) {
   link.click();
 }
 
+// Modal functions
 const openModal = function () {
-  modal.classList.remove("hide");
-  overlay.classList.remove("hide");
+  document.getElementById("modal").classList.remove("hide");
+  document.getElementById("overlay").classList.remove("hide");
 };
 
 const closeModal = function () {
-  modal.classList.add("hide");
-  overlay.classList.add("hide");
+  document.getElementById("modal").classList.add("hide");
+  document.getElementById("overlay").classList.add("hide");
 };
 
-
-if(localStorage.getItem("darkMode") === "enabled"){
-  document.body.classList.add("dark-mode")
+// Dark mode
+if (localStorage.getItem("darkMode") === "enabled") {
+  document.body.classList.add("dark-mode");
 }
-
-
