8aacd6f127a88bd187f729e93552bb91aa69f756
abdul-ajim-khan <aak8389966@gmail.com>
2025-02-25 17:17:39 +0530

    fix modal & loadmore button

--- posts.js
+++ posts.js
@@ -1,18 +1,27 @@
 // window onload function
 window.onload = function () {
   let maincont = document.getElementById("maincont");
+  let load = document.createElement("div");
+  load.classList.add("load");
+  maincont.appendChild(load);
   posts();
 };
 
 let currentPage = 1;
 const limit = 10;
 let postsData = [];
+let users = [];
 
 async function posts() {
   let maincont = document.getElementById("maincont");
-  let users = [];
+  maincont.innerHTML = "";
 
-  //modal / overlay
+  // Show loading animation
+  let load = document.createElement("div");
+  load.classList.add("load");
+  maincont.appendChild(load);
+
+  // Modal / Overlay
   let overlay = document.createElement("div");
   overlay.id = "overlay";
   overlay.classList.add("hide");
@@ -24,57 +33,55 @@ async function posts() {
   closeBtn.innerHTML = `X`;
   modal.appendChild(closeBtn);
 
-
   let exportButton = document.createElement("button");
   exportButton.id = "exportButton";
   exportButton.innerText = "Export to CSV";
-  maincont.appendChild(exportButton);
-
-  exportButton.addEventListener("click", () => {
-    exportToCSV(postsData);
-  });
+  exportButton.addEventListener("click", () => exportToCSV(postsData));
 
   let container = document.createElement("div");
   container.id = "container";
 
-  await fetch("https://dummyjson.com/posts")
+  await fetch(`https://dummyjson.com/posts?limit=${limit}&skip=${(currentPage - 1) * limit}`)
     .then((response) => response.json())
     .then((json) => (users = json.posts));
 
+  // Hide loading animation
   maincont.innerHTML = "";
   maincont.appendChild(exportButton);
   maincont.appendChild(modal);
   maincont.appendChild(overlay);
   maincont.appendChild(container);
 
+  let loadButton = document.createElement("button");
+  loadButton.textContent = "Load more...";
+  loadButton.classList.add("loadButton");
+  loadButton.addEventListener("click", loadMorePosts);
+  maincont.appendChild(loadButton);
 
+  displayPosts(users);
+}
 
-
-  users.forEach(async (user, idx) => {
+function displayPosts(postsArray) {
+  let container = document.getElementById("container");
+  postsArray.forEach(async (user) => {
     let mainUserdiv = document.createElement("div");
     mainUserdiv.classList.add("mainUserdiv");
     container.appendChild(mainUserdiv);
+
     let userDiv = document.createElement("div");
     userDiv.classList.add("userDiv");
     mainUserdiv.appendChild(userDiv);
 
-   
-
-    let userName = await infos(user.id);
+    let userName = await infos(user.userId);
     let title = user.title;
     let body = user.body;
     let tags = user.tags;
     let likes = user.reactions.likes;
     let dislikes = user.reactions.dislikes;
-    
-    userDiv.addEventListener("click", openModal);
-    overlay.addEventListener("click", closeModal);
-    closeBtn.addEventListener("click", closeModal);
 
     userDiv.addEventListener("click", openModal);
     overlay.addEventListener("click", closeModal);
     closeBtn.addEventListener("click", closeModal);
-   
 
     let titleDiv = document.createElement("div");
     titleDiv.classList.add("titlediv");
@@ -83,9 +90,7 @@ async function posts() {
 
     let bodyDiv = document.createElement("div");
     bodyDiv.classList.add("bodydiv");
-    const words = body.split(" ");
-    const first20Words = words.slice(0, 20).join(" ");
-    bodyDiv.innerText = first20Words + "...";
+    bodyDiv.innerText = body.split(" ").slice(0, 20).join(" ") + "...";
     userDiv.appendChild(bodyDiv);
 
     let reaction = document.createElement("div");
@@ -96,13 +101,11 @@ async function posts() {
     likesDiv.classList.add("likesdiv");
     likesDiv.innerText = `Likes: ${likes}`;
     reaction.appendChild(likesDiv);
-    likesDiv.innerText = `Likes: ${likes}`;
 
     let dislikesDiv = document.createElement("div");
     dislikesDiv.classList.add("dislikesdiv");
     dislikesDiv.innerText = `Dislikes: ${dislikes}`;
     reaction.appendChild(dislikesDiv);
-    dislikesDiv.innerText = `dislikes: ${dislikes}`;
 
     let tagsDiv = document.createElement("div");
     tagsDiv.classList.add("tagsdiv");
@@ -110,46 +113,26 @@ async function posts() {
     userDiv.appendChild(tagsDiv);
 
     postsData.push({
-      title: title,
-      userName: userName,
-      bodyPreview: first20Words + "...",
-      likes: likes,
-      dislikes: dislikes,
+      title,
+      userName,
+      bodyPreview: body.split(" ").slice(0, 20).join(" ") + "...",
+      likes,
+      dislikes,
       tags: tags.join(", "),
     });
   });
-
 }
 
-//user data from user id
 async function infos(user_id) {
-  let user_info = await fetch(`https://dummyjson.com/users/${user_id}`).then(
-    (response) => response.json()
-  );
-
-  return {
-    fullname: `${user_info.firstName} ${user_info.lastName}`,
-    email: user_info.email,
-    username: user_info.username,
-    gender: user_info.gender,
-    role: user_info.role,
-  };
+  let user_info = await fetch(`https://dummyjson.com/users/${user_id}`).then((response) => response.json());
+  return `${user_info.firstName} ${user_info.lastName}`;
 }
 
-//csv data download
 function exportToCSV(data) {
-  const csvHeaders = [
-    "Title",
-    "User Name",
-    "Body Preview",
-    "Likes",
-    "Dislikes",
-    "Tags",
-  ];
+  const csvHeaders = ["Title", "User Name", "Body Preview", "Likes", "Dislikes", "Tags"];
   const csvRows = data.map((item) => {
     return `"${item.title}","${item.userName}","${item.bodyPreview}","${item.likes}","${item.dislikes}","${item.tags}"`;
   });
-
   const csvContent = [csvHeaders.join(","), ...csvRows].join("\n");
   const blob = new Blob([csvContent], { type: "text/csv" });
   const link = document.createElement("a");
@@ -157,32 +140,22 @@ function exportToCSV(data) {
   link.download = "posts_data.csv";
   link.click();
 }
-//modal portion
-const openModal = function () {
+
+const openModal = () => {
   document.getElementById("modal").classList.remove("hide");
   document.getElementById("overlay").classList.remove("hide");
 };
 
-const closeModal = function () {
+const closeModal = () => {
   document.getElementById("modal").classList.add("hide");
   document.getElementById("overlay").classList.add("hide");
 };
 
-//dark mode
-if (localStorage.getItem("darkMode") === "enabled") {
-  document.body.classList.add("dark-mode");
-}
-// show more user function
-let user_index = 0;
-function showMoreUsers() {
-  for (let i = user_index; i < user_index + 10 && i < users.length; i++) {
-    let userr = document.getElementById(`user_index-${i}`);
-    if (userr) {
-      userr.style.display = "block";
-    }
-  }
-  user_index += 10;
-  if (user_index === users.length) {
-    loadButton.style.display = "none";
-  }
+async function loadMorePosts() {
+  currentPage++;
+  let morePosts = [];
+  await fetch(`https://dummyjson.com/posts?limit=${limit}&skip=${(currentPage - 1) * limit}`)
+    .then((response) => response.json())
+    .then((json) => (morePosts = json.posts));
+  displayPosts(morePosts);
 }
