e3444044846a30a6c13e28bfb4d2b29b4f3497ec
soudip-maity <soudipmaity605@gmail.com>
2025-02-28 12:24:50 +0530

    commit

--- sidebar.js
+++ sidebar.js
@@ -2,7 +2,7 @@ let mode = document.getElementById("mode");
 let maindiv = document.getElementById("maindiv");
 let container = document.getElementById("container");
 let dark_mode = document.getElementsByClassName("dark-mode")
-
+let tooltip = document.getElementsByClassName("tooltip")
 
 
 
@@ -25,6 +25,7 @@ document.addEventListener("DOMContentLoaded", function () {
 
     if(localStorage.getItem("darkMode") === "enabled"){
         maindiv.classList.toggle("inner_dark_mode")
+
       }
 
 
