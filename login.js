bd135ab9f4f3b11e49baa293b36787d14be33bbb
abdul-ajim-khan <aak8389966@gmail.com>
2025-03-03 13:04:44 +0530

    login page & register page

---
+++ login.js
@@ -0,0 +1,123 @@
+let email_F = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
+let m = localStorage.getItem("em")
+let mh = localStorage.getItem("ps")
+let login = document.getElementById("login")
+let login_Email = document.getElementById("login_Email")
+let login_pass = document.getElementById("login_pass")
+login.onclick=()=>{
+    if(email_F.test(login_Email.value)){
+        if (m.includes(login_Email.value) && mh.includes(login_pass.value)) {
+            window.location.href = "users.html";
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
